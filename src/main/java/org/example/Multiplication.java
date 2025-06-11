df98cc6b2a2da47714ea1fc014961d7ffe7cf16a
Ben <bendanderson13@gmail.com>
2025-06-11 12:11:02 -0500

    initial commit, exceptional case testing not yet working

---
+++ src/main/java/org/example/Multiplication.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Multiplication {
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+}
---
+++ src/test/java/org/example/MultiplyTests.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MultiplyTests {
+
+    @Test
+    public void testMultiplyPositiveCase() {
+        assertEquals(2, Multiplication.multiply(1, 2));
+        assertEquals(0, Multiplication.multiply(0, 0));
+    }
+
+    @Test
+    public void testMultiplyFailingCase() {
+        assertNotEquals(4, Multiplication.multiply(-1, -1));
+    }
+
+    @Test
+    public void testExceptionalCase() {
+         assertThrows(ArithmeticException.class, () -> { Multiplication.multiply(Integer.MAX_VALUE, 1); });
+    }
+}
